cb89ee9fa77ee863aba7f2a179eaa565242f89bf
l0quend0 <n00407782@upn.pe>
2024-11-13 14:12:13 -0500

    first commit

---
+++ Lista/Desayuno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    internal class Desayuno
+    {
+        public string Nombre { get; set; }
+        public int Unnecesary { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
